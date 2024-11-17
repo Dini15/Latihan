1b506b3fbef4f8bb5f6abf8f00708a39bfbb5575
Dini15 <dinia.sutrisno15@gmail.com>
2024-11-17 18:52:51 +0700

    ini revisi syntax

--- Latihan_LC3.py
+++ Latihan_LC3.py
@@ -28,18 +28,24 @@ def data_by_index(id: int):
     # else:
     return {"data": row.to_dict()}
     # data.to_dict(orient='records')
-    
+
+print(data.head())
+
 #endpoint untuk menghapus data berdasarkan index
 @app.delete("/delete/{index}")
-def delete_data (index: int):
-    # global def
+def delete_data(index: int):
+    data = pd.read_csv("handled_data.csv")
     #mengecek apakah index valid
+    #mengecek index sebelum dihapus
+    print(f"Jumlah data sebelum penghapusan: {len(data)}")
+
     if index < 0 or index >= len(data):
         raise HTTPException(status_code=404, detail="Data tidak ditemukan")
     
     #menghapus entry data dari DataFrame
-    data = data.drop(index, index=True)
-    data.reset_index(drop=True, inplace=True)
+    
+    data = data.drop(data.index[index])
+    data.reset_index(drop=True,inplace=True)
 
     #mengembalikan peubahan ke file csv
     data.to_csv("handled_data.csv", index=False)
